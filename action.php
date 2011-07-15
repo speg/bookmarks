5c639c8d2891b5bf3af2a0e4a891ad4e6af6ca5a
speg <speggy@gmail.com>
2011-07-15 16:40:48 -0400

    oops

--- action.php
+++ action.php
@@ -8,11 +8,11 @@ $bookmarks = new Bookmark();
 switch($action){
 	case 'delete':
 		$bookmarks->delete($_REQUEST['id']);
-		header('Location: http://test.dev/bookmarks/marks.php');
+		header('Location: http://shiemstr.dev/bookmarks/marks.php');
 		break;
 	case 'like':
 		$bookmarks->like($_REQUEST['id']);
-		header('Location: http://test.dev/bookmarks/marks.php');
+		header('Location: http://shiemstr.dev/bookmarks/marks.php');
 		break;
 	case 'add':
 		$bookmarks->add($_REQUEST['name'],$_REQUEST['url']);
